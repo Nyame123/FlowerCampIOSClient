922a90c7da7c65191d58ce20bdc8d89a36a0e208
nyame <nyamebismark12.nb@gmail.com>
2024-07-27 22:59:07 +0000

    Initial Commit

---
+++ FlowerCampIOSClient/FlowerCampIOSClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerCampIOSClientApp.swift
+//  FlowerCampIOSClient
+//
+//  Created by Nyame Bismark on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlowerCampIOSClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
